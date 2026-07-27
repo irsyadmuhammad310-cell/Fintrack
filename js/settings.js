17f965304e7b9b584c58092473a6d3bd0c8872ab
irsyadmuhammad310-cell <irsyadmuhammad310@gmail.com>
2026-07-27 13:33:55 +0800

    Update settings.js

--- js/settings.js
+++ js/settings.js
@@ -1,7 +1,7 @@
 // === SETTINGS (v15.7) ===
 let setSubTab = 'profile';
 
-const FINTRACK_VERSION = 'v15.8.0';
+const FINTRACK_VERSION = 'v15.8.1';
 
 function renderSettings(c) {
   if (window.innerWidth <= 768) { renderMobileSettings(c); return; }
