0e7f801306a62ebe08c6e871d95f94a2179c9996
MIrsyad <irsyadmuhammad310@gmail.com>
2026-09-25 18:33:53 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // === FinTrack Premium Service Worker (V2.0.3) ===
 // IMPORTANT: Bump this version string on EVERY deploy to trigger update
-const CACHE_NAME = 'fintrack-v2.0.4-b1790311436';
+const CACHE_NAME = 'fintrack-v2.0.4-b1790330105';
 
 // Listen for skip waiting message from the app
 self.addEventListener('message', function(e) {
